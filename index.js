1b681e20b40269a5e6000e7bc77312a0fe6a764c
jignesh sharma <jigneshsharma9868@gmail.com>
2023-09-27 13:49:09 +0530

    home ejs setup just now

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const port = 3000;
+const path = require('path');
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
+
+app.get('/',(req,res)=> {
+    res.send("<h1>this is render by expressjs</h1>")
+}); 
 
 
 app.listen(port, (err)=> {
