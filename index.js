d36f150beedcc3029ef7c5f23443d788edadd0a5
jignesh sharma <jigneshsharma9868@gmail.com>
2023-09-28 18:42:07 +0530

    just learned how to use body parser for fetching the user data and storing in to the array of the list

--- index.js
+++ index.js
@@ -6,9 +6,47 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
 
+app.use(express.urlencoded({extended:true}));
+
+
+//creating a dummy contact list 
+var contactList = [
+    {
+        "name" : "jignesh_sharma",
+        "phone" : 34020924324,
+    },
+    {
+        name : "something_else",
+        "phone" : 3324093240,
+    },
+    {
+        name: "newContact",
+        "phone" : 342049320,
+    }
+];
+
 app.get('/',(req,res)=> {
-    res.send("<h1>this is render by expressjs</h1>")
-}); 
+    return res.render('home',{
+
+    "title" : "Contact_list",
+    contact_list : contactList,
+});
+ 
+});  
+app.get('/practice',(req,res)=> {
+    res.render("practice",{
+        title : "welcome to the page"
+    });
+})
+app.post('/create-contact',(req,res)=> {
+    // contactList.push({
+    //     name : req.body.name,
+    //     phone : req.body.phone
+    // });
+    contactList.push(req.body);
+
+    return res.redirect('/');
+})
 
 
 app.listen(port, (err)=> {
